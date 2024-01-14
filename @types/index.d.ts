7165b93affb4cec55655ce7a3e2118694836a9bf
Borozan-Evghenii <borozan.eugen@gmail.com>
2024-01-14 10:37:52 +0200

    feat: add global types for project add types for api currency and rate response and integrate her in endpoints function

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,2 +1,41 @@
-import { ComponentProps } from 'react';
-export type Props<T> = ComponentProps<T>
+import { ComponentPropsWithRef } from 'react'
+export type Props<T> = import('react').ComponentPropsWithRef<T>;
+
+interface ServerData {
+  status: number;
+  terms: string;
+  privacy: string;
+}
+
+interface ServerCurrenciesSymbols extends ServerData {
+  currencies: {
+    [key: string]: string
+  };
+}
+
+interface ServerCurrenciesRate extends ServerData {
+  timestamp: number;
+  source: string;
+  quotes: {
+    [key: string]: number
+  };
+}
+
+interface ServerCurrenciesHistorical extends ServerData {
+  historical: boolean,
+  date: string,
+  timestamp: number;
+  source: string;
+  quotes: {
+    [key: string]: number
+  }
+}
+
+type CurrencySymbol = {
+  name: string
+  symbol: string
+}
+
+type CurrencyRate = {
+  [key: string]: number
+}

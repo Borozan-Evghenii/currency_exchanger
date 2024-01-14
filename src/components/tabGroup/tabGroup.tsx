ad1b31d1dea52853a80d63e0fb2e755fdb3a0630
Borozan-Evghenii <borozan.eugen@gmail.com>
2024-01-14 10:43:55 +0200

    refactor: onSelectAutocomplete function from tabGroup.tsx refactor method for find selected item in popular list

--- src/components/tabGroup/tabGroup.tsx
+++ src/components/tabGroup/tabGroup.tsx
@@ -1,60 +1,76 @@
 import React from 'react';
 import Tab from '../tab/Tab.tsx';
-import { FaAngleDown } from "react-icons/fa";
+import { FaAngleDown } from 'react-icons/fa';
 import Autocomplete from '../autocomplet/Autocomplete.tsx';
+import { CurrencySymbol } from '../../../@types';
 
-interface TabGroupProps{
-  onSelect : (item:string) => void
-  value: string
+interface TabGroupProps {
+  onSelect: (item: CurrencySymbol) => void;
+  value: string;
+  listItem: CurrencySymbol[] | undefined;
+
 }
-const TabGroup: React.FC<TabGroupProps> = ({onSelect, value}) => {
-  const [select , setSelect ] = React.useState<string>(value || 'UAH')
-  const [showAutocomplete, setShowAutocomplete] = React.useState<boolean>(false)
-  const [selectedAutocomplete, setSelectAutocomplete] = React.useState<string| null>(null)
 
+const TabGroup: React.FC<TabGroupProps> = ({ onSelect, value, listItem }) => {
+  const [select, setSelect] = React.useState<string | undefined>(value || 'UAH');
+  const [showAutocomplete, setShowAutocomplete] = React.useState<boolean>(false);
+  const [selectedAutocomplete, setSelectAutocomplete] = React.useState<string | null>(null);
+  const autocompleteRef = React.useRef<HTMLDivElement>(null)
+  const popular: CurrencySymbol[] = [
+    { symbol: 'USD', name: 'US Dollar' },
+    { symbol: 'EUR', name: 'Euro' },
+    { symbol: 'MDL', name: 'Moldovan Leu' },
+    { symbol: 'RUB', name: 'Russian Ruble' },
+    { symbol: 'RON', name: 'Romanian Leu' }
+  ];
 
-
-
-  const data: string[] = ['UAH', 'USD', 'EUR', 'RON', 'MDL']
-
+  const onSelectAutocomplete = (item: CurrencySymbol) => {
+    setSelect(item.symbol);
+    if (popular?.some(someItem => someItem.symbol === item.symbol)) {
+      setSelectAutocomplete(null);
+      onSelect(item);
+    } else {
+      setSelectAutocomplete(item.symbol);
+      onSelect(item);
+    }
+    setShowAutocomplete(prevState => !prevState);
+  }
+  const onTabSelect = (item: CurrencySymbol) => {
+    onSelect(item);
+    setSelect(item.symbol);
+    setShowAutocomplete(false);
+  }
 
   return (
-    <div className='flex my-[10px] relative'>
+    <div className="flex my-[10px] relative">
       {
-        data?.map((item, index) => (
+        popular?.map((item, index) => (
           <Tab
-            isActive={select === item}
-            onClick={() => {
-              onSelect(item)
-              setSelect(item);
-            }}
+            isActive={select === item.symbol}
+            onClick={() => onTabSelect(item)}
             key={index}
-          >{item}</Tab>
+          >{item.symbol}</Tab>
         ))
       }
-      {selectedAutocomplete && (<Tab isActive={select === selectedAutocomplete}>{selectedAutocomplete}</Tab>)}
+      {selectedAutocomplete && (
+        <Tab
+          onClick={() => setShowAutocomplete(prevState => !prevState)}
+          isActive={select === selectedAutocomplete}
+        >{selectedAutocomplete}</Tab>
+      )}
       <Tab
-        onClick={()=> setShowAutocomplete(prevState => !prevState)}
+        onClick={() => setShowAutocomplete(prevState => !prevState)}
       >
         <FaAngleDown />
       </Tab>
-      {showAutocomplete && (<Autocomplete
-        initialValue={''}
-        onSelect={(item) => {
-          setSelect(item)
-          if(!data.includes(item)){
-            setSelectAutocomplete(item)
-            onSelect(item)
-          }else{
-
-            onSelect(item)
-          }
-
-
-
-          setShowAutocomplete(prevState => !prevState)
-        }}
-      />)}
+      {showAutocomplete && (
+        <Autocomplete
+          ref={autocompleteRef}
+          items={listItem}
+          initialValue={''}
+          onSelect={(item) => onSelectAutocomplete(item)}
+        />
+      )}
     </div>
   );
 };

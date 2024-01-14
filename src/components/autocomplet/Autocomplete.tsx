c5468337d229d461906cdd65883bb42bd6f99439
Borozan-Evghenii <borozan.eugen@gmail.com>
2024-01-14 10:34:01 +0200

    refactor: Autocomplete.tsx style: fix on top search input add type for useState and other function add useDeferValue for filtration

--- src/components/autocomplet/Autocomplete.tsx
+++ src/components/autocomplet/Autocomplete.tsx
@@ -1,51 +1,54 @@
 import React, { useDeferredValue } from 'react';
 import style from './Autocomplete.module.css';
 import { CiSearch } from 'react-icons/ci';
-interface AutocompleteProps {
+import { CurrencySymbol } from '../../../@types';
+interface AutocompleteProps{
   initialValue: string;
-  onSelect: (item: string) => void;
+  onSelect: (item: CurrencySymbol) => void;
+  items: CurrencySymbol[] | undefined
 }
 
-const Autocomplete: React.FC<AutocompleteProps> = ({ onSelect, initialValue }) => {
+const Autocomplete= React.forwardRef<HTMLDivElement, AutocompleteProps>(({ onSelect, initialValue,items }, ref) => {
 
-
-  const list = [{ symbol: 'LEU', name: 'Leu Moldovenesc' }, { symbol: 'UAH', name: 'Grivna Ucraineasca' }];
-
-  const [filteredList, setFilteredList] = React.useState(list);
-
+  const [filteredList, setFilteredList] = React.useState<CurrencySymbol[] |undefined>(items);
   const [selectedItem, setSelectedItem] = React.useState<string >(initialValue );
 
   const deferList = useDeferredValue(filteredList);
   const inputChangeHandler = (target: string) => {
-    setFilteredList(list.filter((item) => item.symbol.toLowerCase().includes(target.toLowerCase()) || item.name.toLowerCase().includes(target.toLowerCase())));
+    setFilteredList(items?.filter((item) => item.symbol.toLowerCase().includes(target.toLowerCase()) || item.name.toLowerCase().includes(target.toLowerCase())));
   };
   const itemClick = (item: {name:string, symbol: string}) => {
     setSelectedItem(item.symbol);
-    onSelect(item.symbol);
+    onSelect(item);
   };
 
+
   return (
-    <div className={style.container}>
-      <div className={style.inputContainer}>
-        <input
-          onChange={(event) => inputChangeHandler(event.target.value)}
-          className={style.input}
-        />
-        <CiSearch />
+    <div className={style.container} ref={ref}>
+      <div className='relative'>
+        <div className={style.inputContainer}>
+          <input
+            autoFocus={true}
+            onChange={(event) => inputChangeHandler(event.target.value)}
+            className={style.input}
+          />
+          <CiSearch />
+        </div>
+        <ul className={style.list}>
+          {
+            deferList?.map(item => (
+              <li
+                key={item.symbol}
+                onClick={() => itemClick(item)}
+                className={`${style.listItem} ${selectedItem === item.symbol ? 'bg-blue-500 text-[#fff]' : ''}`}
+              >{item.symbol} - {item.name}</li>
+            ))
+          }
+        </ul>
       </div>
-      <ul className={style.list}>
-        {
-          deferList?.map(item => (
-            <li
-              key={item.symbol}
-              onClick={() => itemClick(item)}
-              className={`${style.listItem} ${selectedItem === item.symbol ? 'bg-blue-500 text-[#fff]' : ''}`}
-            >{item.symbol} - {item.name}</li>
-          ))
-        }
-      </ul>
     </div>
+
   );
-};
+})
 
 export default Autocomplete;
